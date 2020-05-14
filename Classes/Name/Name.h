432064d79e3317d28f125800d8491001448d62a0
jonny <jonnyap@hotmail.it>
2020-05-14 22:57:40 +0200

    test

---
+++ Classes/Name/Name.h
@@ -0,0 +1,29 @@
+
+#ifndef CS_PROJECT_NAME_H
+#define CS_PROJECT_NAME_H
+
+#include <iostream>
+#include<string>
+
+using namespace std;
+
+class Name{
+public:
+    Name();
+    Name(string,string);
+    ~Name();
+
+    Name operator+(const Name&) const;
+    void setNome(string);
+    void setCognome(string);
+
+
+    string getNome();
+    string getCognome();
+
+
+private:
+    string Nome;
+    string Cognome;
+
+};
